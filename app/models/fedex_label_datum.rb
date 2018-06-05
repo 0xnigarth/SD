1548983971750f826ad75d3adc40f1966b451349
Cesar Nava <cnava@icalialabs.com>
2018-06-04 23:57:01 -0500

    Calculated max mins. Overweight boolean and amount ov overweight

--- app/models/fedex_label_datum.rb
+++ app/models/fedex_label_datum.rb
@@ -11,31 +11,109 @@ class FedexLabelDatum < ApplicationRecord
     'fedex'
   end
 
-  def in_inches?
+  def file_in_inches?
+    file_distance_unit == 'IN'
+  end
+
+  def file_in_cm?
+    file_distance_unit == 'CM'
+  end
+
+  def file_in_pounds?
+    file_mass_unit == 'LB'
+  end
+
+  def file_weight_in_kg
+    file_in_pounds? ? file_weight * 0.45359237 : file_weight
+  end
+
+  def file_width_api_in_cm
+    file_in_inches? ? file_width / 0.39370 : file_width
+  end
+
+  def file_height_api_in_cm
+    file_in_inches? ? file_height / 0.39370 : file_height
+  end
+
+  def file_volumetric_weight
+    (file_weight_in_kg * file_width_api_in_cm * file_height_api_in_cm) / 5000
+  end
+
+
+#####################################################################
+  def api_in_inches?
     api_distance_unit == 'IN'
   end
 
-  def in_cm?
+  def api_in_cm?
     api_distance_unit == 'CM'
   end
 
-  def in_pounds?
+  def api_in_pounds?
     api_weight_unit == 'LB'
   end
 
   def api_weight_in_kg
-    in_pounds? ? api_weight * 0.45359237 : api_weight
+    api_in_pounds? ? api_weight * 0.45359237 : api_weight
   end
 
   def api_width_in_cm
-    in_inches? ? api_width / 0.39370 : api_width
+    api_in_inches? ? api_width / 0.39370 : api_width
   end
 
   def api_height_in_cm
-    in_inches? ? api_height / 0.39370 : api_height
+    api_in_inches? ? api_height / 0.39370 : api_height
   end
 
-  def volumetric_weight
-    (api_weight_in_kg * api_width_in_cm * api_height_in_cm) / 5000
+  def api_length_in_cm
+    api_in_inches? ? api_length / 0.39370 : api_length
+  end
+
+  def api_volumetric_weight
+    (api_length_in_cm * api_width_in_cm * api_height_in_cm) / 5000
+  end
+
+  def ceil_weights
+    {'file_weight_in_kg': file_weight_in_kg.ceil, 'file_volumetric_weight': file_volumetric_weight.ceil, 'api_volumetric_weight': api_volumetric_weight.ceil, 'api_weight_in_kg': api_weight_in_kg.ceil}
+  end
+
+  def max_weight_txt
+    ceil_weights.sort_by { |name, value| value }[0][0]
+  end
+
+  def file_weights
+    {'file_weight_in_kg': file_weight_in_kg.ceil, 'file_volumetric_weight': file_volumetric_weight.ceil}
+  end
+
+  def api_weights
+    {'api_volumetric_weight': api_volumetric_weight.ceil, 'api_weight_in_kg': api_weight_in_kg.ceil}
+  end
+
+  def max_weight_api
+api_weights.sort_by { |name, value| value }[0][0]
+  end
+
+  def max_weight_file
+file_weights.sort_by { |name, value| value }[0][0]
+  end
+
+  def max_weight_file_value
+    file_weights[max_weight_file]
+  end
+
+  def max_weight_api_value
+    api_weights[max_weight_api]
+  end
+
+
+
+  def has_over_weight?
+    max_weight_api_value > max_weight_file_value
+  end
+
+
+
+  def amount_of_over_weight
+    (max_weight_file_value - max_weight_api_value).abs
   end
 end
